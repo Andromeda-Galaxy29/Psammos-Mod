0479abd6042c5806e830548547f57250e44293b8
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2023-12-27 23:56:52 +0300

    Fixing attributes

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+Attribute.add("ferric-stone");
 Events.on(ContentInitEvent, e => {
     Vars.content.block("ferric-stone").attributes.add(Attribute.get("ferric-stone"), parseFloat(1));
     Vars.content.block("ferric-craters").attributes.add(Attribute.get("ferric-stone"), parseFloat(1));
