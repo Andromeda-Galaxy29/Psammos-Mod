157920fe772809e84c470498d0c8e702538939c1
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2023-12-28 00:09:33 +0300

    Changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 Attribute.add("ferric-stone");
 Events.on(ContentInitEvent, e => {
-    Vars.content.block("ferric-stone").attributes.set(Attribute.get("ferric-stone"), 0.5);
-    Vars.content.block("ferric-craters").attributes.set(Attribute.get("ferric-stone"), 0.5);
+    Vars.content.block("ferric-stone").attributes.set(Attribute.get("ferric-stone"), 0.11111111);
+    Vars.content.block("ferric-craters").attributes.set(Attribute.get("ferric-stone"), 0.11111111);
 });
