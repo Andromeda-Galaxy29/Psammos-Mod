481ab63ecda3d583852ed8fb67ded297103c378a
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2023-12-28 00:04:05 +0300

    Fixed crash

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 Attribute.add("ferric-stone");
 Events.on(ContentInitEvent, e => {
-    Vars.content.block("ferric-stone").attributes.add(Attribute.get("ferric-stone"), parseFloat(1));
-    Vars.content.block("ferric-craters").attributes.add(Attribute.get("ferric-stone"), parseFloat(1));
-    Vars.content.block("ferric-stone-wall").attributes.add(Attribute.get("ferric-stone"), parseFloat(1));
+    Vars.content.block("ferric-stone").attributes.set(Attribute.get("ferric-stone"), parseFloat(1));
+    Vars.content.block("ferric-craters").attributes.set(Attribute.get("ferric-stone"), parseFloat(1));
 });
