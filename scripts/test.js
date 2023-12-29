732bdf15febb89a426a6129af3942f83b7545697
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2023-12-29 17:26:05 +0300

    Test

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,11 @@
-Events.run(Trigger.update, () => {
-    Groups.build.each(b => {
-        if(b.block == Vars.content.block("psammos-1a-sieve")){
-            let x = Math.floor(b.x/8)
-            let y = Math.floor(b.y/8)
-            Vars.world.tile(x, y).setNet(Vars.content.block("titanium-wall-large"));
-        }
-    })
+Events.on(WorldLoadEvent, event => {
+    Timer.schedule(e => {
+        Groups.build.each(b => {
+            if(b.block == Vars.content.block("psammos-1a-sieve")){
+                let x = Math.floor(b.x/8)
+                let y = Math.floor(b.y/8)
+                Vars.world.tile(x, y).setNet(Vars.content.block("titanium-wall-large"));
+            }
+        })
+    }, 0, 60);
 });
