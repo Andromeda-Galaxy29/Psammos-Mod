0ca463cb379715d7241b0da7e3aa100be273f1f8
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2023-12-29 17:12:50 +0300

    Test

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,6 @@
 Events.run(Trigger.update, () => {
     Groups.build.each(b => {
-        if(b.block == Vars.content.block("silicon-smelter")){
+        if(b.block == Vars.content.block("psammos-1a-sieve")){
             let x = Math.floor(b.x/8)
             let y = Math.floor(b.y/8)
             Vars.world.tile(x, y).setNet(Vars.content.block("titanium-wall"));
