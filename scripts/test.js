a16f3b30176ffaae4b029aa4ad460fd5fd3f04aa
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2023-12-29 17:03:09 +0300

    Test

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,7 @@
-Events.on(WorldLoadEvent, event => {
-    Timer.schedule(e => {
-        Groups.build.each(b => {
-            if(b.block == Vars.content.block("copper-wall")){
-                Vars.world.tile(b.x, b.y).setNet(Vars.content.block("titanium-wall"))
-            }
-        })
-    }, 0, 1);
+Events.run(Trigger.update, () => {
+    Groups.build.each(b => {
+        if(b.block == Vars.content.block("copper-wall")){
+            Vars.world.tile(b.x, b.y).setNet(Vars.content.block("titanium-wall"));
+        }
+    })
 });
