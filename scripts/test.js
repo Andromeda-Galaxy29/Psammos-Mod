d78ff065382dcbad938c5fce9dc5f7a0c3965bfb
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2023-12-29 17:53:55 +0300

    Test

--- scripts/test.js
+++ scripts/test.js
@@ -2,9 +2,7 @@ Events.on(WorldLoadEvent, event => {
     Timer.schedule(e => {
         Groups.build.each(b => {
             if(b.block == Vars.content.block("psammos-1a-sieve")){
-                let x = Math.floor(b.x/8)
-                let y = Math.floor(b.y/8)
-                Fx.shockwave.at(x, y)
+                Fx.shockwave.at(b.x, b.y)
             }
         })
     }, 0, 3);
