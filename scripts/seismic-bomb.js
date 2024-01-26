32e79ed0131fa1bb36d873698a8fbef6bb042fc9
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-01-26 20:42:57 +0300

    Fixed everything exploding

--- scripts/seismic-bomb.js
+++ scripts/seismic-bomb.js
@@ -1,4 +1,6 @@
 Events.on(BlockBuildEndEvent, event => {
-    Damage.damage(event.tile.x*8, event.tile.y*8, 6 * 8, 60);
-    Fx.instBomb.at(event.tile.x*8, event.tile.y*8)
+    if (event.tile.block == Vars.content.block("psammos-3a-seismic-bomb")){
+        Damage.damage(event.tile.x*8, event.tile.y*8, 6 * 8, 60);
+        Fx.instBomb.at(event.tile.x*8, event.tile.y*8)
+    }
 });
