e1dd161468bc322ece1afdb954af2ec144017f22
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-01-26 20:39:35 +0300

    Test

--- scripts/seismic-bomb.js
+++ scripts/seismic-bomb.js
@@ -1,4 +1,4 @@
 Events.on(BlockBuildEndEvent, event => {
-    Damage.damage(event.tile.x, event.tile.y, 6 * 8, 60);
-    Fx.instBomb.at(event.tile.x, event.tile.y)
+    Damage.damage(event.tile.x*8, event.tile.y*8, 6 * 8, 60);
+    Fx.instBomb.at(event.tile.x*8, event.tile.y*8)
 });
