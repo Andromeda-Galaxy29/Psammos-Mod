110cb52a6cc4742b1706abf955b61d3204085e70
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-01-26 20:46:24 +0300

    Fixed seismic bombs

--- scripts/seismic-bomb.js
+++ scripts/seismic-bomb.js
@@ -1,5 +1,5 @@
 Events.on(BlockBuildEndEvent, event => {
-    if (event.tile.block == Vars.content.block("psammos-3a-seismic-bomb")){
+    if (event.tile.block() == Vars.content.block("psammos-3a-seismic-bomb")){
         Damage.damage(event.tile.x*8, event.tile.y*8, 6 * 8, 60);
         Fx.instBomb.at(event.tile.x*8, event.tile.y*8)
     }
