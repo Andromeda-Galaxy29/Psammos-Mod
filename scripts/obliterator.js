d74ee5b262fc0e5f83352c4aa922ebdce464380d
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-02-11 14:18:26 +0300

    Update obliterator

--- scripts/obliterator.js
+++ scripts/obliterator.js
@@ -1,9 +1,17 @@
-const ConsumeNotBlast = class ConsumeNotBlast extends ConsumeItemFilter{
-    constructor(){
-        filter = item => item != Vars.content.item("blast-compound")
+Events.on(WorldLoadEvent, event => {
+    Timer.schedule(e => {
+        Groups.build.each(b => {obliterator(b)})
+    }, 0, 1);
+});
+
+function obliterator(b){
+    if(Vars.state.isPaused()){
+        return
+    }
+    if(b.block == Vars.content.block("psammos-Zz-obliterator") && b.items.has(Vars.content.item("blast-compound"))){
+        Fx.smokeCloud.at(b.x, b.y)
+        for(let item of Vars.content.items()){
+            b.items.remove(item, 1)
+        }
     }
 }
-
-Events.on(ContentInitEvent, e => {
-    Vars.content.block("psammos-Zz-obliterator").consume(new ConsumeNotBlast())
-});
