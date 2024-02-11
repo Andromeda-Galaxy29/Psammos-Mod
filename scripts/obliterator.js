2351ee14f44a0795d2fe9e948f6590db8d154f91
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-02-11 15:02:35 +0300

    Update obliterator

--- scripts/obliterator.js
+++ scripts/obliterator.js
@@ -1,19 +1,20 @@
 Events.on(WorldLoadEvent, event => {
     Timer.schedule(e => {
         Vars.world.tiles.each((x, y) => {
-            let b = Vars.world.tile(x, y).build
-            if(b != null){
-                obliterator(b)
+            let tile = Vars.world.tile(x, y)
+            if(tile.block() == Vars.content.block("psammos-Zz-obliterator")){
+                obliterator(tile)
             }
         })
     }, 0, 1);
 });
 
-function obliterator(b){
+function obliterator(tile){
     if(Vars.state.isPaused()){
         return
     }
-    if(b.block == Vars.content.block("psammos-Zz-obliterator") && b.items.has(Vars.content.item("blast-compound"))){
+    let b = tile.build
+    if(b.items.has(Vars.content.item("blast-compound"))){
         Fx.smokeCloud.at(b.x, b.y)
         b.items.clear()
     }
