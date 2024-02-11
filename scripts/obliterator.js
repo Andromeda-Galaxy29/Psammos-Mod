debfafe283591b715d5ff15165fb5d12e20fbef4
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-02-11 16:45:31 +0300

    Update obliterator

--- scripts/obliterator.js
+++ scripts/obliterator.js
@@ -1,4 +1,5 @@
-const ConsumeAny = extend(ConsumeItemFilter, {filter: item => true})
+const ConsumeAny = ConsumeItemFilter
+ConsumeAny.filter = item => true
 
 Events.on(ContentInitEvent, e => {
     Vars.content.block("psammos-Zz-obliterator").consume(new ConsumeAny())
