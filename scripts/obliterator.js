841f71f817424a8efd83645b42b08a72921481b9
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-02-11 17:36:15 +0300

    Update

--- scripts/obliterator.js
+++ scripts/obliterator.js
@@ -1,3 +1,3 @@
 Events.on(ContentInitEvent, e => {
-    print(Vars.content.block("psammos-Zz-obliterator").consume(new ConsumeItemFilter(item => true)))
+    Vars.content.block("psammos-Zz-obliterator").consume(new ConsumeItemFilter(Boolf(item => true)))
 });
