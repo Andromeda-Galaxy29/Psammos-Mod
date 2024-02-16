740b4b11a5fbdf5717c7c1d01d40b09a98f7aae8
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-02-16 23:34:02 +0300

    update

---
+++ scripts/obliterator.js
@@ -0,0 +1,21 @@
+Events.on(WorldLoadEvent, event => {
+    Timer.schedule(e => {
+        Groups.build.each(b => {f(b)})
+    }, 0, 1);
+});
+
+function f(b){
+    if(b.block == Vars.content.block("psammos-Zz-obliterator")){
+        let result = false
+        for(let i of Vars.content.items){
+            if(i == Vars.content.item("blast-compound")){
+                continue
+            }
+            if(b.items.has(i)){
+                result = true
+                break
+            }
+        }
+        b.enabled = result
+    }
+}
