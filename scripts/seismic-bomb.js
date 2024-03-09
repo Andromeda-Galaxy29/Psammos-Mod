cc05478d7123d933f5d0fb093b0640d4babcbeab
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-03-09 17:19:09 +0300

    Update oil deposits

--- scripts/seismic-bomb.js
+++ scripts/seismic-bomb.js
@@ -4,9 +4,10 @@ Events.on(BlockBuildEndEvent, event => {
         Fx.instBomb.at(event.tile.x*8, event.tile.y*8)
         for(let x=event.tile.x-1; x<=event.tile.x+1; x++){
             for(let y=event.tile.y-1; y<=event.tile.y+1; y++){
-                if (Vars.world.tile(x, y).overlay() == Vars.content.block("psammos-1c-oil-deposit")){
+                if (Vars.world.tile(x, y).floor() == Vars.content.block("psammos-3d-oil-deposit")){
                     Vars.world.tile(x, y).setFloorNet(Vars.content.block("tar"))
                     Vars.world.tile(x, y).setOverlayNet(Vars.content.block("air"))
+                    Vars.world.tile(x, y).setNet(Vars.content.block("air"))
                 }
             }
         }
