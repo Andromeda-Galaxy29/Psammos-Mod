0ec81515878b7ffc44aaf1ed02fd7b6977bd78ee
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-03-10 12:36:05 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ Events.on(ContentInitEvent, e => {
     Vars.content.block("psammos-1b-detonation-drill").drillEffect = new MultiEffect(
         Fx.mineImpact,
         Fx.drillSteam,
-        Fx.dynamicSpikes.wrap(Items.blastCompound.color, 30f),
-        Fx.mineImpactWave.wrap(Items.blastCompound.color, 45f)
+        Fx.dynamicSpikes.wrap(Items.blastCompound.color, 30),
+        Fx.mineImpactWave.wrap(Items.blastCompound.color, 45)
     );
 });
