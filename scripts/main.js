8aedf91ef06604e28f94090a888e19dfe30146f9
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-03-10 12:32:14 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
 require("attributes");
 require("seismic-bomb");
+
+Events.on(ContentInitEvent, e => {
+    Vars.content.block("psammos-1b-detonation-drill").drillEffect = new MultiEffect(
+        Fx.mineImpact,
+        Fx.drillSteam,
+        Fx.dynamicSpikes.wrap(Items.blastCompound.color, 30f),
+        Fx.mineImpactWave.wrap(Items.blastCompound.color, 45f)
+    );
+});
