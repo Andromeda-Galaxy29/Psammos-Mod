0a96ba2355a2d85310646344abbe477a4ede5e30
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-11-02 13:51:35 +0300

    Added clear campaign buttons to settings
    Thank you, developers of Subvoyage for the code

--- src/psammos/PsammosMod.java
+++ src/psammos/PsammosMod.java
@@ -1,15 +1,21 @@
 package psammos;
 
 import arc.*;
+import arc.struct.*;
 import arc.util.*;
 import mindustry.*;
+import mindustry.content.TechTree;
+import mindustry.ctype.UnlockableContent;
+import mindustry.game.*;
+import mindustry.gen.*;
 import mindustry.type.*;
-import mindustry.ui.fragments.MenuFragment;
+import mindustry.ui.fragments.*;
 import psammos.content.*;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
-import psammos.graphics.PsammosMenuRenderer;
+import psammos.graphics.*;
+import psammos.ui.*;
 
 import static arc.Core.*;
 import static mindustry.Vars.*;
@@ -82,6 +88,38 @@ public class PsammosMod extends Mod{
         ui.settings.addCategory(bundle.get("setting.psammos-category"), "psammos-settings-icon", t -> {
             t.checkPref("psammos-warning", true);
             t.checkPref("psammos-custom-menu", true);
+
+            // Thank you, developers of Subvoyage, for this code
+            t.pref(new ButtonPref(bundle.get("setting.psammos-clear-tech-tree"),Icon.trash,() -> {
+                ui.showConfirm("@confirm", bundle.get("setting.psammos-clear-tech-tree.confirm"), () -> {
+                    PsammosPlanets.psammos.techTree.reset();
+                    for(TechTree.TechNode node : PsammosPlanets.psammos.techTree.children){
+                        node.reset();
+                    }
+                    content.each(c -> {
+                        if(c instanceof UnlockableContent u && c.minfo != null && c.minfo.mod != null && c.minfo.mod.name.equals("psammos")){
+                            u.clearUnlock();
+                        }
+                    });
+                    settings.remove("unlocks");
+                });
+            }));
+            t.pref(new ButtonPref(bundle.get("setting.psammos-clear-campaign"),Icon.trash,() -> {
+                ui.showConfirm("@confirm", bundle.get("setting.psammos-clear-campaign.confirm"), () -> {
+                    Seq<Saves.SaveSlot> toDelete = Seq.with();
+                    control.saves.getSaveSlots().each(s -> {
+                        if(s.getSector() == null) return;
+                        if(s.getSector().planet == PsammosPlanets.psammos) {
+                            toDelete.add(s);
+                            Log.info("Deleted Psammos sector: "+ s.getSector().id+ (s.getSector().preset != null ? " "+s.getSector().preset.localizedName : ""));
+                        }
+                    });
+                    toDelete.each(Saves.SaveSlot::delete);
+                    ui.showInfoOnHidden(bundle.get("setting.psammos-clear-campaign-close.confirm"), () -> {
+                        Core.app.exit();
+                    });
+                });
+            }));
         });
     }
 }
