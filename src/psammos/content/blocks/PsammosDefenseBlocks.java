13cb38e27c2c05591dee9edc30f026efb22e59e9
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2026-07-08 15:57:25 +0300

    Didn't seem to work

--- src/psammos/content/blocks/PsammosDefenseBlocks.java
+++ src/psammos/content/blocks/PsammosDefenseBlocks.java
@@ -112,8 +112,7 @@ public class PsammosDefenseBlocks {
             range = 20 * Vars.tilesize;
             nodeShieldRadius = 3f * Vars.tilesize;
             linkShieldThickness = 2.5f * Vars.tilesize;
-            radiationRequirements = new Seq<>();
-            radiationRequirements.add(new RadiationStack(PsammosRadTypes.light, 4.5f));
+            radiationRequirements = Seq.with(new RadiationStack(PsammosRadTypes.light, 4.5f));
         }};
     }
 }
