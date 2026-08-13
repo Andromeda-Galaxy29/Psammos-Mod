c294a924bcb0bba8651b3ff283bb2f814c80cdce
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2026-08-13 21:31:23 +0300

    Update HugCommandAI.java

--- src/psammos/ai/HugCommandAI.java
+++ src/psammos/ai/HugCommandAI.java
@@ -1,17 +1,23 @@
 package psammos.ai;
 
+import arc.util.Log;
 import mindustry.ai.types.CommandAI;
 import mindustry.entities.Sized;
 
 public class HugCommandAI extends CommandAI {
+
     @Override
     public void circleAttack(float circleLength) {
-        if(unit.within(target, (unit.hitSize + (target instanceof Sized s ? s.hitSize() : 1f)) * 0.5f)){
-            //circle target
-            unit.movePref(vec.set(target).sub(unit).rotate(90f).setLength(unit.speed()));
-        }else{
-            //move toward target in a straight line
-            unit.movePref(vec.set(target).sub(unit).limit(unit.speed()));
+        if (target == null) return;
+
+        vec.set(target).sub(unit);
+
+        float attackDst = (unit.hitSize + (target instanceof Sized s ? s.hitSize() : 1f)) * 0.5f;
+        if (unit.within(target, attackDst)) {
+            vec.rotate(90f);
         }
+
+        vec.setLength(prefSpeed());
+        unit.movePref(vec);
     }
 }
